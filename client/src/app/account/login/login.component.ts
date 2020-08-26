9040b097e2bc68f487567dd1a1175a3659c9b3f7
archeonV9 <janiukbartosz@gmail.com>
2020-08-26 23:57:06 +0200

    End of section 17

---
+++ client/src/app/account/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AccountService } from '../account.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  returnUrl: string;
+
+  constructor(private router: Router, private accountService: AccountService, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl || '/shop';
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
+      ]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    this.accountService.login(this.loginForm.value).subscribe(
+      () => {
+        this.router.navigateByUrl(this.returnUrl);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
