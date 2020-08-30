765f67469ff49610fa8b959b03bf2122b471f694
archeonV9 <janiukbartosz@gmail.com>
2020-08-30 14:25:17 +0200

    End of Section 20 Part II

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { BasketService } from './basket.service';
-import { IBasket, IBasketItem } from '../shared/models/basket';
 import { Observable } from 'rxjs';
+import { IBasket, IBasketItem, IBasketTotals } from '../shared/models/basket';
+import { BasketService } from './basket.service';
 
 @Component({
   selector: 'app-basket',
@@ -10,11 +10,13 @@ import { Observable } from 'rxjs';
 })
 export class BasketComponent implements OnInit {
   basket$: Observable<IBasket>;
+  basketTotals$: Observable<IBasketTotals>;
 
   constructor(private basketService: BasketService) { }
 
   ngOnInit() {
     this.basket$ = this.basketService.basket$;
+    this.basketTotals$ = this.basketService.basketTotal$;
   }
 
   removeBasketItem(item: IBasketItem) {
--- client/src/app/orders/orders.module.ts
+++ client/src/app/orders/orders.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders.component';
 import { OrderDetailedComponent } from './order-detailed/order-detailed.component';
 import { OrdersRoutingModule } from './orders-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    OrdersRoutingModule
+    OrdersRoutingModule,
+    SharedModule
   ],
   declarations: [OrdersComponent, OrderDetailedComponent]
 })
